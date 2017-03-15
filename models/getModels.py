ea9b29fe3dae01ecf7ef09eb7df1332a3a7664fb
ezelf <alfasecurity@paranoici.org>
2017-03-15 19:12:38 -0300

    idea

---
+++ models/getModels.py
@@ -0,0 +1,8 @@
+def getAllModels(md):
+	'''
+	# 	Idea
+		Desde esta funcion deberia poder listar todos los modulos individuales que representar a cada modelo.
+
+		El return lo tomaria 'choices=[]', del argumento --model y --show-models
+	'''
+	return md
